1e23af4bf3f5a75d94f7e1cba463489ded543abd
yuxuanwangke <yuxuanwangke@192.168.101.7>
2020-11-02 02:42:00 +0800

    word1&2 and their revs to array lists done.
    Working on comparing.

--- SpellChecker/src/WordRecommender.java
+++ SpellChecker/src/WordRecommender.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
 
 public class WordRecommender {
+	
+	public WordRecommender(String fileName) {}
+	public double getSimilarity(String word1, String word2) {
+	        
+	        ArrayList<Character> word1ArrayList=new ArrayList<Character>();
+	        ArrayList<Character> revword1ArrayList=new ArrayList<Character>();
+	        for(int i=0;i<word1.length();i++)
+	        {
+	        	word1ArrayList.add(word1.charAt(i));
+	        	revword1ArrayList.add(word1.charAt(word1.length() - i -1));
+	        }
+	        System.out.println(word1ArrayList);
+	        System.out.println(revword1ArrayList);
+	        return 2.4;
+	    }
+		
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world! esme check");
+		
 		
 	}
 
-}
