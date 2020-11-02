bbae6ce13043b6df9e4c9e8250aa5de5d1be3de3
dbinstock <dbinstock@DBinstock-PC.sieben.local>
2020-11-01 23:28:53 -0500

    added WordRecommenderTest.java for unit testing

---
+++ SpellChecker/src/WordRecommenderTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WordRecommenderTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetSimilarity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWordSuggestions() {
+		fail("Not yet implemented");
+	}
+
+}
