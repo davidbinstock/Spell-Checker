d714939998246df251501d35f5ffc9bc183f6249
dbinstock <dbinstock@DBinstock-PC.sieben.local>
2020-11-02 19:19:33 -0500

    added Printer class and PrinterTest tester file

---
+++ SpellChecker/src/Printer.java
@@ -0,0 +1,16 @@
+
+public class Printer {
+	
+	private String outputFileName;
+	
+	public Printer(String filename) {
+		outputFileName = filename;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
