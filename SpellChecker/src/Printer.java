ea5e07e6a28347b79401d1107662e3f5b701ae49
dbinstock <dbinstock@DBinstock-PC.sieben.local>
2020-11-02 19:49:28 -0500

    Printer class now has working printNextWord and clearFile methods

--- SpellChecker/src/Printer.java
+++ SpellChecker/src/Printer.java
@@ -1,4 +1,4 @@
-
+import java.io.*;
 public class Printer {
 	
 	private String outputFileName;
@@ -7,9 +7,39 @@ public class Printer {
 		outputFileName = filename;
 	}
 	
+	public void printNextWord(String word) {
+		try {
+			FileWriter fw = new FileWriter(outputFileName, true); //added this in order to get words to append
+			PrintWriter pw = new PrintWriter(fw);
+			pw.print(word + " ");
+			pw.flush();
+		} catch (FileNotFoundException e) {
+			System.out.println("something went wrong with the filename");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	public void clearFile() {
+		try {
+			PrintWriter pw = new PrintWriter(outputFileName);
+			pw.print("");
+			pw.flush();
+		} catch (FileNotFoundException e) {
+			System.out.println("something went wrong with the filename");
+			e.printStackTrace();
+		}
+	}
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Printer p = new Printer("testout.txt");
+		p.clearFile();
+		p.printNextWord("hello");
+		p.printNextWord("what's");
+		p.printNextWord("the");
+		p.printNextWord("Skinny?");
 
 	}
 
